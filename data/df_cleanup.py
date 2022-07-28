3d21da00c9028e3cb58025b26d827d43fbbd58b8
mateusb12 <mateus_b09@hotmail.com>
2022-07-28 18:07:31 -0300

    Entities

--- data/df_cleanup.py
+++ data/df_cleanup.py
@@ -15,10 +15,10 @@ from path_reference.folder_reference import get_data_path
 #     def generate_first_name(self):
 #         self.df["character_first_name"] = self.df["character"].apply(lambda x: x.split(" ", 1)[0])
 
-def cleanup_df(input_df: pd.DataFrame):
-    input_df["character"] = input_df["character"].str.replace("\(.*\)", "")
+def cleanup_df(input_df: pd.DataFrame) -> None:
+    # input_df["character"] = input_df["character"].str.replace("\(.*\)", "")
+    input_df["character"] = input_df["character"].str.replace(r'\(.*\)', "", regex=True)
     input_df["character_first_name"] = input_df["character"].apply(lambda x: x.split(" ", 1)[0])
-    return input_df
 
 
 def __main():
---
+++ nlp/books.py
@@ -0,0 +1,56 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import dataclasses
+import os
+from pathlib import Path
+
+import pandas as pd
+import spacy
+from spacy import displacy
+from spacy.lang import en as english
+from spacy.tokens import Doc
+
+from path_reference.folder_reference import get_data_path
+
+
+class BookAnalyser:
+    def __init__(self):
+        self.NER: english = spacy.load('en_core_web_sm')
+        self.all_books: list[os.DirEntry] = [book for book in os.scandir(get_data_path()) if '.txt' in book.name]
+        self.current_book = None
+
+    def set_book_example(self) -> None:
+        book: os.DirEntry = self.all_books[0]
+        self.current_book = self.__apply_nlp(book)
+
+    def __apply_nlp(self, input_book: os.DirEntry) -> Doc:
+        book_text = open(input_book).read()
+        return self.NER(book_text)
+
+    def print_book_example(self) -> str:
+        book = self.current_book
+        example = self.__apply_nlp(book)
+        return spacy.displacy.render(example[:100], style="ent", jupyter=True, minify=True)
+
+    def get_book_entities(self) -> pd.DataFrame:
+        entity_pot = []
+
+        for index, sentence in enumerate(self.current_book.sents):
+            print(index)
+            if entity_list := [entity.text for entity in sentence.ents]:
+                entity_pot.append({"sentence": sentence, "entities": entity_list})
+        return pd.DataFrame(entity_pot)
+
+
+def __main():
+    book_analyser = BookAnalyser()
+    book_analyser.set_book_example()
+    aux = book_analyser.get_book_entities()
+    print(aux)
+
+
+if __name__ == '__main__':
+    __main()
+
