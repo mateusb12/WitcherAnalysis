74262d10f49a2c8148cac0f0ae0bc7d34ed1a910
mateusb12 <mateus_b09@hotmail.com>
2022-12-10 09:46:54 -0300

    dropdown working

--- gui/interface.py
+++ gui/interface.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 from typing import List
 
+from gui.utils import get_book_folders, get_book_names
+
 
 class BookSelector:
     def __init__(self, window_size: tuple[int, int] = (500, 500)):
@@ -9,23 +11,47 @@ class BookSelector:
         self.root.geometry(f"{window_size[0]}x{window_size[1]}")
         self.window_width = window_size[0]
         self.window_height = window_size[1]
-        self.dropdown = None
-        self.instance_pipeline()
+        self.first_dropdown, self.second_dropdown = None, None
+        self.first_dropdown_selected_option, self.second_dropdown_selected_option = tk.StringVar(), tk.StringVar()
+        self.run()
 
-    def instance_pipeline(self):
-        self.instantiate_dropdown()
+    def run(self):
+        self.instantiate_first_dropdown()
+        self.instantiate_second_dropdown()
         self.root.mainloop()
 
-    def instantiate_dropdown(self):
-        options = ["1", "2", "3", "4", "5"]
-        self.dropdown = ttk.OptionMenu(self.root, tk.StringVar(), *options, command=self.execute_dropdown)
+    def instantiate_first_dropdown(self):
+        options = get_book_folders()
+        first_option = options[0]
+        self.first_dropdown_selected_option.set(first_option)
+        self.first_dropdown = ttk.OptionMenu(self.root, self.first_dropdown_selected_option, first_option, *options,
+                                             command=self.execute_dropdown)
         style = ttk.Style()
         style.theme_use("clam")
-        self.dropdown.pack_configure(side="top", pady=0.07*self.window_height)
+        self.first_dropdown.pack_configure(side="top", pady=0.07 * self.window_height)
 
-    @staticmethod
-    def execute_dropdown(value):
+    def instantiate_second_dropdown(self):
+        first_dropdown_option = self.first_dropdown_selected_option.get()
+        options = get_book_names(first_dropdown_option)
+        first_option = options[0]
+        self.second_dropdown_selected_option.set(first_option)
+        self.second_dropdown = ttk.OptionMenu(self.root, self.second_dropdown_selected_option, first_option, *options)
+        self.second_dropdown.pack_configure(side="top", pady=0.14 * self.window_height)
+
+    def update_second_dropdown_options(self):
+        first_dropdown_option = self.first_dropdown_selected_option.get()
+        options = get_book_names(first_dropdown_option)
+        self.second_dropdown["menu"].delete(0, "end")
+        for option in options:
+            self.second_dropdown["menu"].add_command(label=option,
+                                                     command=lambda value: self.second_dropdown_selected_option.set(
+                                                         value))
+        self.second_dropdown_selected_option.set(options[0])
+
+    def execute_dropdown(self, value):
         print(value)
+        self.first_dropdown_selected_option.set(value)
+        self.update_second_dropdown_options()
         # print(self.dropdown["text"])
 
 
---
+++ gui/utils.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from path_reference.folder_reference import get_books_path
+
+
+def get_book_folders():
+    raw_books_names = [f for f in get_books_path().iterdir() if f.is_dir()]
+    book_type_names = [str(f).split("\\")[-1] for f in raw_books_names]
+    return [book_type_name.replace("_", " ").title() for book_type_name in book_type_names]
+
+
+def get_book_names(folder_name: str):
+    real_folder_name = folder_name.lower().replace(" ", "_")
+    if "_books" not in real_folder_name:
+        real_folder_name += "_books"
+    raw_books_names = [f for f in get_books_path().iterdir() if f.is_dir()]
+    folder_path = [f for f in raw_books_names if str(f).split("\\")[-1] == real_folder_name]
+    folder_content = [f for f in folder_path[0].iterdir() if f.is_file()]
+    book_names = [str(f).split("\\")[-1] for f in folder_content]
+    return [book_name.replace(".txt", "") for book_name in book_names]
+
+
+def __main():
+    print(get_book_names("Harry Potter"))
+
+
+if __name__ == '__main__':
+    __main()
