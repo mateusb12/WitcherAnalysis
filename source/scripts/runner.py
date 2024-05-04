badac580612b4a082c918a66824f9e8620c4911f
Mateus <mateus_b09@hotmail.com>
2024-05-04 17:49:25 -0300

    django installation

--- source/scripts/runner.py
+++ source/scripts/runner.py
@@ -1,11 +1,10 @@
 import pandas as pd
 
 from source.nlp_processing.entity_analysis.book_manager import BookManager
-from source.nlp_processing.entity_analysis.entity_extractor import EntityExtractor
 from source.nlp_processing.entity_filter import EntityFilter
 from source.nlp_processing.model_loader import load_nlp_model
 from source.nlp_processing.relationship_creator import RelationshipCreator
-from witcher_network.node_plot import NodePlot
+from source.witcher_network import NodePlot
 
 
 class Runner:
@@ -15,7 +14,6 @@ class Runner:
         → 2. Filters the entity dataframe, only including character entities
         → 3. Creates the network dataframe, stating who is connected to who
         → 4. Creates the network plot, storing the network in a .html file"""
-        print("Initializing runner...")
         nlp_model = load_nlp_model()
         self.book_analyser = BookManager(nlp_model)
         self.book_analyser.set_series(series)
@@ -24,7 +22,6 @@ class Runner:
         self.node_plot = NodePlot()
         self.book_number: int = 0
         self.book_name = ""
-        print("Runner initialized!")
 
     def set_book(self, book_number: int):
         self.book_analyser.select_book(book_number)
@@ -32,17 +29,16 @@ class Runner:
         self.book_name = self.book_analyser.book_names_dict[self.book_number]
 
     def book_pipeline(self) -> pd.DataFrame:
-        print("Entity dataframe analysis")
+        print("2/3 (Entity processing)")
         entity_df = self.book_analyser.get_book_entities()
         self.entity_filter.set_entity_df(entity_df)
-        print("Creating filtered dataframe")
         filtered_df = self.entity_filter.export_filtered_dataframe()
         self.relationship_creator.set_entity_df(filtered_df)
-        print("Creating network dataframe")
         relationship_df = self.relationship_creator.aggregate_network()
         self.node_plot.set_network_df(relationship_df)
         self.node_plot.pipeline()
         degree_centrality_dict = self.node_plot.degree_dict
+        print("3/3 (Network processing)")
         return pd.DataFrame.from_dict(degree_centrality_dict, orient='index')
 
     def get_centrality(self) -> dict:
@@ -57,7 +53,7 @@ def __main():
     r.set_book(4)
     r.book_pipeline()
     r.plot()
-    print("wow")
+    return
 
 
 if __name__ == "__main__":
